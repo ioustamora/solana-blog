e5280dd162c134936eb2f49c19d21a98dd14dc4c
ioustamora <ioustamora@protonmail.com>
2021-11-04 12:35:41 +0200

    program init

--- programs/solana-blog/src/lib.rs
+++ programs/solana-blog/src/lib.rs
@@ -1,18 +1,37 @@
 use anchor_lang::prelude::*;
+use std::str::from_utf8;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("EmrnGQ7rfeiiEJjwq4F1P5Y28bTB44co6JvpiijVSXG2");
 
 #[program]
 pub mod solana_blog {
     use super::*;
     pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
-        //ctx.accounts
+        let blog_acc = &mut ctx.accounts.blog_account;
+        blog_acc.authority = *ctx.accounts.authority.key;
+
+        Ok(())
+    }
+
+    pub fn make_post(
+        ctx: Context<MakePost>,
+        new_post: Vec<u8>
+    ) -> ProgramResult {
+        let post = from_utf8(&new_post).map_err(|err| {
+            msg!("Invalid UTF-8, from_byte {}", err.valid_up_to());
+            ProgramError::InvalidInstructionData
+        })?;
+        msg!(post);
+
+        let blog_acc = &mut ctx.accounts.blog_account;
+        blog_acc.latest_post = new_post; 
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {
+pub struct Initialize<'info> {
     #[account(
         init,
         payer = authority,
@@ -24,11 +43,20 @@ pub struct Initialize {
     pub blog_account: Account<'info, BlogAccount>,
     //#[account(mut)]
     pub authority: Signer<'info>,
-    pub system_program: Pubkey<'info, System>
+    pub system_program: Program<'info, System>
+}
+
+#[derive(Accounts)]
+pub struct MakePost<'info> {
+    #[account(mut,
+        has_one = authority
+    )]
+    pub blog_account: Account<'info, BlogAccount>,
+    pub authority: Signer<'info>
 }
 
 #[account]
 pub struct BlogAccount {
-    pub latest_post: u64,
+    pub latest_post: Vec<u8>,
     pub authority: Pubkey,
 }
